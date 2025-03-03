e5e53e9199c8118ca31b35682a2a71f3b6e7b564
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-03-03 15:06:35 +0530

    FrontEnd-Setup

---
+++ ChatApp/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='flex justify-center items-center h-screen '>Hello and welcome to the chatApp</h1>
+    </>
+  )
+}
+
+export default App
